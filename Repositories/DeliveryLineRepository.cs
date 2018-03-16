a06b4a8499a71b3e6ec28e92d618027587476d46
CallMeBackdraft <dennis.aspers@gmail.com>
2018-03-16 23:24:14 +0100

    BackEnd work:
    filled deliveryline repo

--- Repositories/DeliveryLineRepository.cs
+++ Repositories/DeliveryLineRepository.cs
@@ -12,24 +12,40 @@ namespace Repositories
 {
     public class DeliveryLineRepository : IDeliveryLineRepository
     {
+        IDeliveryLineContext DeLictx;
+
+        public DeliveryLineRepository()
+        {
+            DeLictx = new DeliveryLineContext();
+        }
         public List<DeliveryLine> GetAllDeliveryLines()
         {
-            throw new NotImplementedException();
+            List<DeliveryLine> result = new List<DeliveryLine>();
+            foreach(DataRow dr in DeLictx.GetAllDeliveryLines().Rows)
+            {
+                result.Add(DataRowToDeliveryLine(dr));
+            }
+            return result;
         }
 
         public List<DeliveryLine> GetAllDeliveryLinesForDelivery(int _deliveryID)
         {
-            throw new NotImplementedException();
+            List<DeliveryLine> result = new List<DeliveryLine>();
+            foreach (DataRow dr in DeLictx.GetAllDeliveryLinesForDelivery(_deliveryID).Rows)
+            {
+                result.Add(DataRowToDeliveryLine(dr));
+            }
+            return result;
         }
 
         public bool SaveDeliveryLine(int _orderId, DeliveryLine _deliveryLine)
         {
-            throw new NotImplementedException();
+            return DeLictx.SaveDeliveryLine(_orderId, _deliveryLine);
         }
 
         private DeliveryLine DataRowToDeliveryLine(DataRow _dataRow)
         {
-            throw new NotImplementedException();
+            return new DeliveryLine(new ProductRepository().GetProductByID(Convert.ToInt16(_dataRow.Field<decimal>("ProductID"))), Convert.ToInt16(_dataRow.Field<decimal>("Amount")));
         }
     }
 }
