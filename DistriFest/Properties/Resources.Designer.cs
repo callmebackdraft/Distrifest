7d8f4bb6202fe72f47d26fd64fe581ec0a4235c0
Sadhonker <bigredfer@gmail.com>
2018-04-21 13:08:19 +0200

    Styling work: - Cried silently in a corner for a while because of the sheer joy of being named in such an awesome project! - styling on Empty Orderform completed, including name splitting, copyright footer, adding logos and colorizing - adjusted copyright footer padding for all pages in Site.css

--- DistriFest/Properties/Resources.Designer.cs
+++ DistriFest/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace DistriFest.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Bospop_logo_zwart {
+            get {
+                object obj = ResourceManager.GetObject("Bospop_logo_zwart", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ChaudfontaineBlauw {
             get {
                 object obj = ResourceManager.GetObject("ChaudfontaineBlauw", resourceCulture);
